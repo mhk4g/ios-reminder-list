f70c08d1231bf2f1d25a2152565bf401726d3754
Matt Kauper <matt.kauper@willowtreeapps.com>
2016-02-20 15:00:02 -0500

    initial commit

---
+++ ReminderList/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ReminderList
+//
+//  Created by Intern on 2/20/16.
+//  Copyright © 2016 Deepa & Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var itemTitle: UITextField!
+    @IBOutlet weak var itemDescription: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
