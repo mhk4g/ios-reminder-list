faafe3dd80f967b8f0430ccf739c3e93d7a0bc11
Matt Kauper <matt.kauper@willowtreeapps.com>
2016-02-20 15:24:49 -0500

    Added text field delegate methods

--- ReminderList/AddReminderViewController.swift
+++ ReminderList/AddReminderViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class AddReminderViewController: UIViewController {
+class AddReminderViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: Properties
 
     @IBOutlet weak var itemTitle: UITextField!
     @IBOutlet weak var itemDescription: UITextField!
+    @IBOutlet weak var exampleLabel: UILabel!
+    var timesClicked: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Assign the view controller as the delegate for both text fields
+        itemTitle.delegate = self
+        itemDescription.delegate = self
     }
 
+    // MARK: UITextFieldDelegate
+
+    // Called first: hide the keyboard
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    // Called second: process the input
+    func textFieldDidEndEditing(textField: UITextField) {
+        NSLog("The user entered input.")
+    }
+
+
+    // MARK: Actions
+
+    @IBAction func createButtonClicked(sender: UIButton) {
+        timesClicked++
+        exampleLabel.text = "Button clicked: \(timesClicked)"
+    }
 
 
 }
